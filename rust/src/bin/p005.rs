b704d44af1b4afb249b8d6f11705946fe824db32
Frans Bol <frans.bol+github@gmail.com>
2019-12-28 21:30:59 +0000

    Rust p005

---
+++ rust/src/bin/p005.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+use euler::math;
+
+fn main() {
+    println!("{}", solve(1..20));
+}
+
+fn solve<T: Iterator<Item=u64>>(seq: T) -> u64 {
+    let mut max_factors: HashMap<u64, u32> = HashMap::new();
+    let mut factors: Vec<_> = seq.flat_map(|n| math::prime_factors(n).into_iter()).collect();
+
+    factors.sort();
+    max_factors.extend(factors);
+
+    max_factors.into_iter().map(|(a,b)| a.pow(b)).product()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solve_1_10() {
+        assert_eq!(solve(1..11), 2520);
+    }
+
+    #[test]
+    fn solve_1_20() {
+        assert_eq!(solve(1..21), 232792560);
+    }
+}
