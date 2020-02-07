8bcb2d9dfe78d1218b41808cda70d1f58ffbf3ff
Frans Bol <frans.bol+github@gmail.com>
2020-02-07 17:30:19 +0000

    Rust p010

---
+++ rust/src/bin/p010.rs
@@ -0,0 +1,28 @@
+use euler::prime;
+
+/// # Summation of primes
+/// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+///
+/// Find the sum of all the primes below two million.
+fn main() {
+    println!("{}", solve(2_000_000));
+}
+
+pub fn solve(limit: u64) -> u64 {
+    prime::iterator().take_while(|&p| p < limit).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn solve_10() {
+        assert_eq!(solve(10), 17);
+    }
+    
+    #[test]
+    fn solve_2m() {
+        assert_eq!(solve(2_000_000), 142_913_828_922);
+    }
+}
