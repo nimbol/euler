76108e4ea6bf3f4665a8c439aea1a2887e710e3e
Frans Bol <frans.bol+github@gmail.com>
2020-02-16 11:40:00 +0000

    Speed up p010 using prime sieve

--- rust/src/bin/p010.rs
+++ rust/src/bin/p010.rs
@@ -8,19 +8,19 @@ fn main() {
     println!("{}", solve(2_000_000));
 }
 
-pub fn solve(limit: u64) -> u64 {
-    prime::iterator().take_while(|&p| p < limit).sum()
+pub fn solve(limit: usize) -> usize {
+    prime::sieve(limit).iter().sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn solve_10() {
         assert_eq!(solve(10), 17);
     }
-    
+
     #[test]
     fn solve_2m() {
         assert_eq!(solve(2_000_000), 142_913_828_922);
